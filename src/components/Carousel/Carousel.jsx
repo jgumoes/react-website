53ee1ffda7d38aadb69b09a2b2b8cbc216c9705c
Jack Gumoes <j.gumoes@gmail.com>
2021-06-11 20:48:00 +0100

    added carousel slides and styling

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,39 @@
+import React from 'react'
+import Swiper from 'react-id-swiper';
+import getCarouselData from "../../req/FetchRequests";
+import './Carousel.css'
 
-const Carousel = () => {
+const SwiperElement = (props) => {
+  const { data } = props
+  const { Title, Subtitle, ImageUrl } = data
+  console.log(Title)
   return(
-    <div className="carousel-container">
-
+    <div className="swiper-element" style={{ backgroundImage: `url(${ImageUrl})`, position: 'center'}}>
+        <h1 className="h">{ Title }</h1>
+        <h3 className="h">{Subtitle}</h3>
+        <div className="button">
+          <b>Contact Us</b>
+        </div>
     </div>
   )
+}
+
+const Carousel = (props) => {
+  const {fetchData} = props
+  console.log(typeof(fetchData))
+  const carouselData = fetchData()
+  const carouselParts = carouselData["Details"]
+  console.log(carouselParts[0])
+
+  return(
+    <div className="carousel-container">
+      <SwiperElement data={carouselParts[0]} />
+    </div>
+  )
+}
+
+Carousel.defaultProps = {
+  fetchData: getCarouselData
 }
 
 export default Carousel
--- src/components/Carousel/Carousel.spec.js
+++ src/components/Carousel/Carousel.spec.js
@@ -1,8 +1,23 @@
 import { mount, shallow } from 'enzyme'
 import Carousel from './Carousel'
+import getCarouselDataMock from "./CarouselSpecHelpers"
 
 describe('Carousel', () => {
+  
   it('should load a div', () => {
     expect(shallow(<Carousel />).exists()).toBe(true)
   });
+
+  it('should call getCarouselData', () => {
+    const mockCall = jest.fn()
+    shallow(<Carousel props={mockCall()} />)
+    expect(mockCall).toHaveBeenCalled()
+  });
+
+  it.todo('should contain 3 child divs'
+  // () => {
+  //   const container = shallow(<Carousel fetchData={getCarouselDataMock()} />)
+  //   expect(container.children()).toHaveLength(3)
+  // }
+  )
 });
