fd14d986a9c4e872effd5d0c1e9cd0cdff35cfb7
Jack Gumoes <j.gumoes@gmail.com>
2021-06-15 18:15:35 +0100

    improved navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,25 +5,30 @@ import {
   Link
 } from "react-router-dom"
 import './NavBar.css'
+import CompanyLogo from '../../Resources/Logo.svg'
 
 const  NavBar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" class="nav-button">
-        <button>
-          HOME
-        </button>
-      </Link>
-      <Link to="/about" class="nav-button">
-        <button>
-          ABOUT US
-        </button>
-      </Link>
-      <Link to="/contact" class="nav-button">
-        <button>
-          CONTACT US
-        </button>
-      </Link>
+      <img id="logo" src={CompanyLogo} alt=""/>
+      <div className="buttons-container">
+        <Link to="/" class="nav-button">
+          <button>
+            HOME
+          </button>
+        </Link>
+        <Link to="/about" class="nav-button">
+          <button>
+            ABOUT US
+          </button>
+        </Link>
+        <Link to="/contact" class="nav-button">
+          <button>
+            CONTACT US
+          </button>
+        </Link>
+        <button id="login-button">Log in</button>
+      </div>
     </nav>
   )
 }
