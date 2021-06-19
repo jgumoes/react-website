53b5c2971dba927ef3cefadd328c2405aa833a30
Jack Gumoes <j.gumoes@gmail.com>
2021-06-19 16:03:16 +0100

    styled about page

--- src/Views/AboutUs/AboutUs.jsx
+++ src/Views/AboutUs/AboutUs.jsx
@@ -7,8 +7,9 @@ const AboutUs = () => {
   return(
     <div className="about-us-container">
       <h3 className="static-text">About us</h3>
-      <p className="static-text"><b>{staticData.firstLine}</b></p>
-      {staticData.firstSectionParagraphs.map((text) => <p className="static-text">{text}</p> )}
+      {/* <p className="static-text" id="first-line"><b>{staticData.firstLine}</b></p> */}
+      {<p className="static-text"><b>{staticData.firstLine}</b><br/>{staticData.firstSectionParagraphs[0]}</p>}
+      {staticData.firstSectionParagraphs.slice(1).map((text) => <p className="static-text">{text}</p> )}
       <div className="image-container">
         <img src={aboutUsImage} alt="" />
       </div>
